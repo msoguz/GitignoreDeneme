243778516bfc0899056d50f193fc4527102b1b63
M. Sener OGUZ <mseneroguz@gmail.com>
2022-08-16 23:11:38 +0300

    B1-

--- src/Gitignore.java
+++ src/Gitignore.java
@@ -4,6 +4,7 @@ public class Gitignore {
         System.out.println("Gitignore Deneme 1");
         System.out.println("Gitignore Deneme 2");
         System.out.println("Gitignore Deneme Branch");
+        System.out.println("Gitignore Deneme Branch 2");
 
 
     }
